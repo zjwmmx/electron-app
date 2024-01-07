c1b500358776e7957ffdd6cdbff99a01dfb4dcf3
zhujinwang <zhujinwang@sunteng.com>
2024-01-07 12:39:44 +0800

    test: update

--- src/main/updater.js
+++ src/main/updater.js
@@ -8,11 +8,14 @@ import { autoUpdater } from 'electron-updater'
 autoUpdater.autoDownload = false
 // autoUpdater.setFeedURL('https://github.com/zjwmmx/electron-app/releases')
 autoUpdater.setFeedURL({
+  url: 'https://github.com/zjwmmx/electron-app/releases',
   provider: "github",
-  owner: "zjwmmx",
-  repo: "electron-app",
-  releaseType: "release",
-  token: "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"
+
+
+  // owner: "zjwmmx",
+  // repo: "electron-app",
+  // releaseType: "release",
+  // token: "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"
 })
 
 autoUpdater.on('error', (error) => {
